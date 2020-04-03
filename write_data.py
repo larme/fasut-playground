25777692be172aa7c6d290bf05d159b25cda8a5b
Zhao Shenyang <dev@zsy.im>
2020-04-03 14:40:03 +0800

    easier to write data.wav

---
+++ write_data.py
@@ -0,0 +1,19 @@
+import struct, wave
+
+data = [i * 0.01 for i in range(10)]
+factor = 2 ** 15
+
+def main():
+    ints = [int(round(v * factor)) for v in data]
+    n = len(ints)
+
+    with wave.open('data.wav', 'wb') as f:
+        f.setnchannels(1)
+        f.setsampwidth(2)
+        f.setframerate(44100)
+        f.setnframes(n)
+        bs = struct.pack('<' + 'h' * n, *ints)
+        f.writeframesraw(bs)
+
+if __name__ == '__main__':
+    main()
